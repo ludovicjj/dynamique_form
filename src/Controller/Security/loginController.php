ac4fa34e9497dc62400750c61370daa5912e866c
ludovicjj <jahanlud@gmail.com>
2024-06-17 09:09:24 +0200

    add login

---
+++ src/Controller/Security/loginController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller\Security;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class loginController extends AbstractController
+{
+    #[Route('/login', name: 'app_login')]
+    public function index(AuthenticationUtils $authenticationUtils): Response
+    {
+        $error = $authenticationUtils->getLastAuthenticationError();
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', [
+            'error' => $error,
+            'last_username' => $lastUsername
+        ]);
+    }
+}
